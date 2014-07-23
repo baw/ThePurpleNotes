1777aeae7fcbbd7062480fa789584bc12e88397c
Brian <iamamthwiz@gmail.com>
2014-07-22 21:18:09 -0700

    add users

---
+++ app/models/user.rb
@@ -0,0 +1,47 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id               :integer          not null, primary key
+#  username         :string(255)      not null
+#  password_diguest :string(255)      not null
+#  session_token    :string(255)      not null
+#  created_at       :datetime
+#  updated_at       :datetime
+#
+
+class User < ActiveRecord::Base
+  before_validations :ensure_session_token
+  
+  validates :username, :password_diguest, :session_token, presence: true
+  
+  def User.find_by_credentials(username, password)
+    user = User.find_by(username: username)
+    
+    if user.is_password(password)
+      user
+    else
+      nil
+    end
+  end
+  
+  def is_password?(password)
+    bcrypt_object = BCrypt::Password.new(self.password_diguest)
+    
+    bcrypt_object.is_password?(password)
+  end
+  
+  def password=(password)
+    self.password_diguest = BCrypt::Password.create(password)
+  end
+  
+  private
+  
+  def ensure_session_token
+    self.session_token || self.generate_session_token
+  end
+  
+  def generate_session_token
+    SecureRandom.urlsafe_base64(16)
+  end
+end
