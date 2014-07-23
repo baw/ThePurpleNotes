12e1e2a9f06c6249b4c1f67c16f9f4464b326958
Brian <iamamthwiz@gmail.com>
2014-07-23 00:02:17 -0700

    set up backbone

--- app/assets/javascripts/evernote.js
+++ app/assets/javascripts/evernote.js
@@ -1,9 +1,14 @@
+/*global Evernote */
 window.Evernote = {
   Models: {},
   Collections: {},
   Views: {},
   Routers: {},
   initialize: function() {
-    alert('Hello from Backbone!');
+    Evernote.Collections.notebooks = new Evernote.Collections.Notebooks();
+    new Evernote.Routers.Router({
+      $rootEl: $("#main")
+    });
+    Backbone.history.start();
   }
 };
