c0349995918a7e0645e5242983977c32532489ef
Brian <iamamthwiz@gmail.com>
2014-07-22 22:09:28 -0700

    add home page

---
+++ app/controllers/static_controller.rb
@@ -0,0 +1,5 @@
+class StaticController < ApplicationController
+  def root
+    render :root
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users
   resource :session
+  
+  root to: "static#root"
 end
