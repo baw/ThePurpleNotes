4df8644df128e61bb65fb55a9745ab072391f6f7
Brian <iamamthwiz@gmail.com>
2014-07-27 15:07:30 -0700

    restyled new tag input

--- app/assets/javascripts/views/tags/new.js
+++ app/assets/javascripts/views/tags/new.js
@@ -1,13 +1,29 @@
 /*global Evernote, JST */
 Evernote.Views.TagsNew = Backbone.View.extend({
+  className: "tagForm fa fa-plus",
   events: {
-    "submit": "submitForm"
+    "submit": "submitForm",
+    "focus #tagName": "newTagFocus",
+    "blur #tagName": "newTagBlur"
   },
   tagName: "form",
   template: JST["tags/new"],
   
   initialize: function (options) {
     this.note = options.note;
+  },
+  
+  newTagBlur: function (event) {
+    var $target = $(event.target);
+    $target.removeClass("tagNameExpanded");
+    $target.parent().addClass("fa fa-plus");
+  },
+  
+  newTagFocus: function (event) {
+    var $target = $(event.target);
+    $target.val("");
+    $target.addClass("tagNameExpanded");
+    $target.parent().removeClass("fa fa-plus");
   },
   
   render: function () {
@@ -28,6 +44,6 @@ Evernote.Views.TagsNew = Backbone.View.extend({
       "note_id": this.note.id
     });
     
-    $tagName.val("");
+    $tagName.val("").blur();
   }
 });
