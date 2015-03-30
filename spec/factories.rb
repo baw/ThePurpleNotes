ea6462814f2ee69fa1dd8f54b467ab29e02a6ec1
Brian Weiser <iamamthwiz+GitHub@gmail.com>
2015-03-29 17:58:07 -0700

    add factories for notebook and note

--- spec/factories.rb
+++ spec/factories.rb
@@ -3,4 +3,15 @@ FactoryGirl.define do
     username { Faker::Internet.user_name }
     password { Faker::Internet.password }
   end
+  
+  factory :note do
+    title { Faker::Name.title }
+    content { Faker::Lorem.paragraph(2) }
+    association :notebook, factory: :notebook
+  end
+  
+  factory :notebook do
+    title { Faker::Name.title }
+    association :user, factory: :user
+  end
 end
